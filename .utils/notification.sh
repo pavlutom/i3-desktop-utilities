ee36cb0ee4eae3d8c64ff2f4583610fbb70bdcf4
Tomas Pavlusek <pavlutom@fit.cvut.cz>
2020-05-14 09:24:40 +0200

    added notification script

---
+++ .utils/notification.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+TEXT="$1"
+SOUND="$2"
+
+test $1 || TEXT="Time's up!"
+test $2 || SOUND="/home/tomesh/.config/sounds/notifications/deduction.ogg"
+
+dunstify "$TEXT"
+paplay "$SOUND"
+
+
